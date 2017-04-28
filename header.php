8c912987c2770a57f8707de08719dd24aee21eea
Joshua Lapid <lapidjoshua@yahoo.com>
2017-04-28 08:15:19 -0700

    testing wordpress blog loop and body class

--- header.php
+++ header.php
@@ -8,6 +8,15 @@
 
     </head>
 
-    <body>
+    <?php
+
+        if( is_front_page() ):
+            $halphen_classes = array( 'awesome-class', 'my-class' );
+        else:
+            $halphen_classes = array( 'not-awesome-class' );
+        endif;
+    ?>
+
+    <body <?php body_class( $halphen_classes ) ?>>
 
         <?php wp_nav_menu( array('theme_location' => 'primary' ) ); ?>
